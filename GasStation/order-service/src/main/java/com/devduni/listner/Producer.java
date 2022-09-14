c99ebee9af9343c7394fa3beb8c66713d51eaed2
BhagyaDevduni <106392592+BhagyaDevduni@users.noreply.github.com>
2022-09-14 23:01:28 +0530

    Update Producer Java

--- GasStation/order-service/src/main/java/com/devduni/listner/Producer.java
+++ GasStation/order-service/src/main/java/com/devduni/listner/Producer.java
@@ -1,21 +1,20 @@
 package com.devduni.listner;
 
+import com.devduni.event.OrderEvent;
 import com.google.gson.Gson;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
-import java.awt.*;
-
-@Slf4j
+@Service
 public class Producer {
 
     public static final String topic = "Fuel Order";
     @Autowired
     private KafkaTemplate<String,String> kafkaTemplate;
 
-    public void topic(Event message) {
-        log.info("topic " + topic);
+    public void toTopic(OrderEvent message) {
+        System.out.println("publish to : " + topic);
         Gson gson=new Gson();
         String newMessage=gson.toJson(message);
         this.kafkaTemplate.send(topic, newMessage);
