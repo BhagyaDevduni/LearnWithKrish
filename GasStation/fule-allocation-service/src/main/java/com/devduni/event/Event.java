b40381992fec225d05cbc5a60edb0c1638c1a3cd
BhagyaDevduni <106392592+BhagyaDevduni@users.noreply.github.com>
2022-09-15 00:19:47 +0530

    Update Event

--- GasStation/fule-allocation-service/src/main/java/com/devduni/event/Event.java
+++ GasStation/fule-allocation-service/src/main/java/com/devduni/event/Event.java
@@ -8,11 +8,11 @@ import lombok.NoArgsConstructor;
 public class Event {
 
 
-    private String name;
+   private String name;
+    private String status;
     private String orderId;
     private String stockId;
-    private String status;
     private  String key;;
-    private  String result;
     private String qty;
+    private  String result;
 }
